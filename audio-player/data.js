cff8e6b0dc061aa87519d091406a103edc382239
dashakarelova <karelova_dasha@mail.ru>
2023-09-28 18:05:48 +0300

    feat: implement js for audio player

---
+++ audio-player/data.js
@@ -0,0 +1,16 @@
+let songs = [
+    {
+        name: 'Don\'t Hurt Yourself',
+        path: 'assets/audio/Beyoncé - Don\'t Hurt Yourself.mp3',
+        artist: 'Beyoncé',
+        cover: 'assets/jpg/Beyonce-Dont_Hurt_Your_self.jpg',
+        back: 'assets/jpg/Beyonce_blur.jpg'
+    },
+    {
+        name: 'Don\'t Start Now',
+        path: 'assets/audio/Dua Lipa - Don\'t Start Now.mp3',
+        artist: 'Dua Lipa',
+        cover: 'assets/jpg/Dua_Lipa-Dont_Start_Now.jpg',
+        back: 'assets/jpg/Dua_Lipa_blur.jpg'
+    }
+]
